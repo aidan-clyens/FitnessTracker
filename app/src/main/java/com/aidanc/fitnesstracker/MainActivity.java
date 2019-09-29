0109a629f8d48571ce98df7c0abf46cefae8cba0
Aidan Clyens <aidanc@rogers.com>
2019-09-29 12:18:52 -0400

    Made window fullscreen

--- app/src/main/java/com/aidanc/fitnesstracker/MainActivity.java
+++ app/src/main/java/com/aidanc/fitnesstracker/MainActivity.java
@@ -8,6 +8,7 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
+import android.view.WindowManager;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
@@ -33,6 +34,9 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
+        getSupportActionBar().hide();
 
         workoutTableHandler = new WorkoutTableHandler(this);
 
